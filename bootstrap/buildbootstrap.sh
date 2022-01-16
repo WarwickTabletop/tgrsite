9922696303f79517f9056b832db9554b1930bcc3
Finnbar Keating <finnjkeating@gmail.com>
2022-01-16 21:36:32 +0000

    Actually update source.scss, document scss

--- bootstrap/buildbootstrap.sh
+++ bootstrap/buildbootstrap.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+mkdir -p build
 if ! sassc "$1" "./build/$1" >&2; then
     exit 1
 fi
 postcss "./build/$1" --no-map --use autoprefixer cssnano -o "$2" >&2
+
+# NOTE: you will need postcss-cli, cssnano and autoprefixer from npm (install globally), and sassc.
+# To run:
+# cd tgrsite/bootstrap
+# ./buildbootstrap.sh source.scss target.css
