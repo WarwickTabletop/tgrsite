390ed9c2757e9e792ac8ce1ed9a1b7f155ade6a3
Anna <44057980+the-Bruce@users.noreply.github.com>
2022-03-05 18:08:31 +0000

    Simplify count

--- votes/templatetags/vote_tags.py
+++ votes/templatetags/vote_tags.py
@@ -13,13 +13,9 @@ register = template.Library()
 def sanitized_vote_count(vote: Election):
     value = vote.votes().count()
     if value <= 5:
-        return "0-5"
+        return "<=5"
     else:
-        seed = hashlib.sha1((str(vote.id) + str(vote.name) + str(value)).encode('utf-8'))
-        generator = random.Random()
-        generator.seed(seed)
-        fuzzed = generator.randrange(math.floor(value * 0.8), math.ceil(value * 1.2))
-        return f"{math.floor(fuzzed * 0.8)}-{math.ceil(fuzzed * 1.2)}"
+        return f"{math.floor(value/5)*5}-{math.ceil(value/5)*5}"
 
 
 @register.simple_tag(takes_context=True)
