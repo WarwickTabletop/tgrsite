f7fd1777f99b81b9d860936b0b1914a2d02acfef
Anna <44057980+the-Bruce@users.noreply.github.com>
2023-05-28 15:46:49 +0100

    Update admin pages to use searches for all member fields

--- newsletters/admin.py
+++ newsletters/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Newsletter
 
+class NewsletterAdmin(admin.ModelAdmin):
+    fields = ('title','body','author','pub_date','summary','ispublished','banner')
+    autocomplete_fields = ('author',)
+
 # Register your models here.
-admin.site.register(Newsletter)
+admin.site.register(Newsletter, NewsletterAdmin)
